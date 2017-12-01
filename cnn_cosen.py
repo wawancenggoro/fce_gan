969e02dfe180c31b5269aba3849b8bca34c6b73f
Wawan Cenggoro <wawancenggoro@gmail.com>
2017-12-02 06:04:19 +0700

    add cosen cnn weights

--- cnn_cosen.py
+++ cnn_cosen.py
@@ -57,66 +57,6 @@ reg_val = 5e-6
 
 def normalize_pixel(data):
     return data/255-.5
-
-def load_data(dataset):
-    if dataset=='mnist':
-        # load data
-        (X_train, y_train), (X_test, y_test) = mnist.load_data()
-        #IPython.embed()
-        # reshape to be [samples][pixels][width][height]
-        X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
-        X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
-        #IPython.embed()
-        
-        # normalize inputs from 0-255 to 0-1
-        X_train = X_train / 255
-        X_test = X_test / 255
-        X_val = X_test
-        # one hot encode outputs
-        y_train = np_utils.to_categorical(y_train)
-        y_test = np_utils.to_categorical(y_test)
-        y_val = y_test
-        num_classes = y_test.shape[1]
-        input_shape=(28, 28, 1)
-    elif dataset=='CelebA':
-        # load data CelebA
-        f=h5py.File('/mnt/Storage/Projects/data/CelebAHDF5/celeba_aligned_cropped.hdf5','r')
-        X_train=f['features'][0:1000]
-        X_val=f['features'][1000:1100]
-        X_test=f['features'][1100:1200]
-        y_train=f['targets'][0:1000]
-        y_val=f['targets'][1000:1100]
-        y_test=f['targets'][1100:1200]
-        
-        
-        # normalize inputs from 0-255 to 0-1
-        X_train = X_train / 255
-        X_test = X_test / 255
-        num_classes = y_test.shape[1]
-        input_shape=(218, 178, 3)
-    elif dataset=='CelebAHDF5':
-        # load data CelebA
-        X_data = HDF5Matrix('/mnt/Storage/Projects/data/CelebAHDF5/celeba_aligned_cropped_normalized.hdf5', 'features')
-        y_data = HDF5Matrix('/mnt/Storage/Projects/data/CelebAHDF5/celeba_aligned_cropped_normalized.hdf5', 'targets')
-#        X_train=X_data[0:162770]
-#        X_val=X_data[162770:182637]
-#        X_test=X_data[182637:202599]
-#        y_train=y_data[0:162770]
-#        y_val=y_data[162770:182637]
-#        y_test=y_data[182637:202599]
-        X_train=X_data
-        X_val=X_data
-        X_test=X_data
-        y_train=y_data
-        y_val=y_data
-        y_test=y_data
-        
-        # normalize inputs from 0-255 to 0-1
-#        X_train = X_train / 255
-#        X_test = X_test / 255
-        num_classes = y_test.shape[1]
-        input_shape=(218, 178, 3)
-    return (X_train, y_train), (X_val, y_val), (X_test, y_test), num_classes, input_shape
     
 def hdf5_generator(dataset,set_type):
     i=0    
@@ -184,19 +124,6 @@ def load_data_attr(dataset):
         
     return num_classes, input_shape
 
-# load data CelebA
-#f=h5py.File('../../data/CelebAHDF5/celeba_aligned_cropped.hdf5','r')
-#X_train=f['features'][0:1000]
-#X_test=f['features'][1000:1100]
-#y_train=f['targets'][0:1000]
-#y_test=f['targets'][1000:1100]
-#
-#
-## normalize inputs from 0-255 to 0-1
-#X_train = X_train / 255
-#X_test = X_test / 255
-#num_classes = y_test.shape[1]
-
 class ClassDependentCost(Layer):
 
     def __init__(self, output_dim, trainable, **kwargs):
@@ -210,6 +137,7 @@ class ClassDependentCost(Layer):
         self.kernel = self.add_weight(name='kernel', 
                                       shape=(input_shape[1],),
                                       initializer='he_uniform',
+                                      regularizer=regularizers.l2(1e-1),
                                       trainable=self.trainable)
         super(ClassDependentCost, self).build(input_shape)  # Be sure to call this somewhere!
 
@@ -224,25 +152,8 @@ def concat_diff(i): # batch discrimination -  increase generation diversity.
     bv = Lambda(lambda i:K.mean(K.abs(i[:] - K.mean(i,axis=0)),axis=-1,keepdims=True))(i)
     i = Concatenate()([i,bv])
     return i
- 
-def baseline_model_api(input_shape):
-    inputs = Input(shape=input_shape)
-    x=Conv2D (32, (5, 5))(inputs)
-    x=Activation('relu')(x)
-    x=MaxPooling2D(pool_size=(2, 2))(x)
-    x=Dropout(0.2)(x)
-    x=Flatten()(x)
-    x=Dense(128)(x)
-    x=Activation('relu')(x)
-    x=Dense(num_classes)(x)
-    predictions=Activation('softmax')(x)
-    model = Model(inputs=inputs, outputs=predictions)    
-#    model.compile(optimizer='adam',
-#              loss='categorical_crossentropy',
-#              metrics=['accuracy'])
-    return model
 
-def gan_dis_model():
+def cosen_cnn_model(type):
     inp = Input(shape=(218,178,3))
     i = inp
 
@@ -254,7 +165,12 @@ def gan_dis_model():
             i = BatchNormalization(name='bn'+name)(i)
         i = Activation('relu',name='relu'+name)(i)
         return i
-        
+    
+    if type=='cost':
+        cost_trainable = True
+    elif type=='normal':
+        cost_trainable = False
+
     i = conv(i,ndf*1,4,std=2,name='0',usebn=False)
     i = concat_diff(i)
     i = conv(i,ndf*2,4,std=2,name='1')
@@ -273,9 +189,14 @@ def gan_dis_model():
     # i = Dense(200, kernel_initializer='he_uniform', kernel_regularizer=regularizers.l2(reg_val), bias_regularizer=regularizers.l2(reg_val),name='dense2')(i)
     # i = Activation('relu',name='relu_dens2')(i)
     i = Dense(num_classes, kernel_initializer='he_uniform', kernel_regularizer=regularizers.l2(reg_val), bias_regularizer=regularizers.l2(reg_val),name='last_dense')(i)
+    i=ClassDependentCost(5,trainable=cost_trainable)(i)
     predictions=Activation('sigmoid')(i)
-    predictions=ClassDependentCost(5,trainable=True)(predictions)
-    model = Model(inputs=inp, outputs=predictions)  
+    model = Model(inputs=inp, outputs=predictions) 
+
+    if type=='cost':
+        for i in range(35):
+            model.layers[i].trainable = False
+
     
     return model
 
@@ -289,16 +210,23 @@ numpy.random.seed(seed)
 num_classes, input_shape = load_data_attr('CelebA_cls5')
 # build the model
 
-model = gan_dis_model()
+model = cosen_cnn_model('normal')
+model_cost = cosen_cnn_model('cost')
 #model = baseline_model_api(input_shape)
 model.summary()
+model_cost.summary()
 filepath="cnn_weights_best.hdf5"
+filepath_routine="cnn_weights.hdf5"
+filepath_routine_cost="cnn_cost_weights.hdf5"
 # model=load_model(filepath)
 # Compile model
 # opt = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=1e-6)
 #opt = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.97)
-opt = optimizers.SGD(lr=0.045, momentum=0.9, decay=0.9)
+opt = optimizers.SGD(lr=0.001, momentum=0, decay=0)
 model.compile(optimizer=opt,
+          loss=losses.binary_crossentropy,
+          metrics=['accuracy'])
+model_cost.compile(optimizer=opt,
           loss=losses.binary_crossentropy,
           metrics=['accuracy'])
 
@@ -310,27 +238,33 @@ def schedule(epoch):
 
           
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-early_stop = EarlyStopping(monitor='val_loss', patience=100, verbose=1, mode='auto')
+checkpoint_routine = ModelCheckpoint(filepath_routine, monitor='val_acc', verbose=1, save_best_only=False, mode='max')
 csv_logger = CSVLogger('training.log', append=False)
 tensorboard = TensorBoard(log_dir='./tf-logs')
 lr_schedule = LearningRateScheduler(schedule)
-callbacks_list = [checkpoint, csv_logger, tensorboard]
+callbacks_list = [checkpoint, checkpoint_routine, csv_logger, tensorboard]
 
-#IPython.embed()
-## Fit the model
-#model.fit(X_train[0:162770], y_train[0:162770], validation_data=(X_val[162770:182637], y_val[162770:182637]), epochs=2, batch_size=200, verbose=2)
-
-## Final evaluation of the model
-#scores = model.evaluate(X_test[182637:202599], y_test[182637:202599], verbose=0)
-#print("Baseline Error: %.2f%%" % (100-scores[1]*100))
-#prediction=model.predict(X_test[202599])
-
+checkpoint_routine_cost = ModelCheckpoint(filepath_routine_cost, monitor='val_acc', verbose=1, save_best_only=False, mode='max')
+callbacks_list_cost = [checkpoint_routine_cost]
 # Fit the model with generator
 # CelebA data size: train = 162770, valid = 19867, test = 19962
 # CelebA cls5 data size: train = 122077, valid = 15138, test = 14724
 # callbacks = [TensorBoard(log_dir='./tf-logs')]
 batch_size=256
-result = model.fit_generator(hdf5_generator('CelebA','train_cls5'),122077//batch_size,2,initial_epoch=0,validation_data=hdf5_generator('CelebA','valid_cls5'),validation_steps=15138//batch_size,callbacks=callbacks_list)
+initial_epoch = 0
+# cost_train_step=122077//batch_size
+# cost_valid_step=15138//batch_size
+cost_train_step=1
+cost_valid_step=1
+for i in range(1000):
+    if i!=0:
+        model_cost.load_weights(filepath_routine)
+    history_cost = model_cost.fit_generator(hdf5_generator('CelebA','train_cls5'),cost_train_step,i+initial_epoch+1,initial_epoch=i+initial_epoch,validation_data=hdf5_generator('CelebA','valid_cls5'),validation_steps=cost_valid_step,callbacks=callbacks_list_cost)
+    callbacks_list_cost = [checkpoint_routine_cost, history_cost]
+
+    model.load_weights(filepath_routine_cost)
+    history = model.fit_generator(hdf5_generator('CelebA','train_cls5'),122077//batch_size,i+initial_epoch+1,initial_epoch=i+initial_epoch,validation_data=hdf5_generator('CelebA','valid_cls5'),validation_steps=15138//batch_size,callbacks=callbacks_list)
+    callbacks_list = [checkpoint, checkpoint_routine, csv_logger, tensorboard, history]
 
 # Final evaluation of the model with generator
 scores = model.evaluate_generator(hdf5_generator('CelebA','test_cls5'),74)
